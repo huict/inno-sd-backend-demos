97cb6768e749d9a84485cbda9e2b6b6dc3f503a8
Hugo-Helder <59727184+Hugo-Helder@users.noreply.github.com>
2024-03-21 11:50:41 +0100

    Updated gRPC to spring boot 3 without tomcat and springboot hacks
    (only netty bind issues remains)

--- rpc/hellogrpc/client/src/main/java/nl/hu/ict/inno/hellogrpc/client/GrpcConfig.java
+++ rpc/hellogrpc/client/src/main/java/nl/hu/ict/inno/hellogrpc/client/GrpcConfig.java
@@ -11,11 +11,11 @@ import org.springframework.context.annotation.Configuration;
         net.devh.boot.grpc.client.autoconfigure.GrpcClientMetricAutoConfiguration.class,
         net.devh.boot.grpc.client.autoconfigure.GrpcClientHealthAutoConfiguration.class,
         net.devh.boot.grpc.client.autoconfigure.GrpcClientSecurityAutoConfiguration.class,
-        net.devh.boot.grpc.client.autoconfigure.GrpcClientTraceAutoConfiguration.class,
+//        net.devh.boot.grpc.client.autoconfigure.GrpcClientTraceAutoConfiguration.class,
         net.devh.boot.grpc.client.autoconfigure.GrpcDiscoveryClientAutoConfiguration.class,
 
         net.devh.boot.grpc.common.autoconfigure.GrpcCommonCodecAutoConfiguration.class,
-        net.devh.boot.grpc.common.autoconfigure.GrpcCommonTraceAutoConfiguration.class,
+//        net.devh.boot.grpc.common.autoconfigure.GrpcCommonTraceAutoConfiguration.class,
 })
 public class GrpcConfig {
 }
